25764a898abb7b44642d3d74e4836c41b5da0138
Alexis Yannuzzi <yannuzzi367@gmail.com>
2021-07-28 13:54:19 -0500

    module3.1

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
@@ -21,15 +23,27 @@ public class MortgageCalculator {
         return interestRate / 12;
     }
 
-    public double calculateMonthlyPayment(){
+    public void calculateMonthlyPayment(){
         long P = loanAmount;
         float r = getMonthlyInterestRate();
         int n = getNumberOfPayments();
-        System.out.println("P=" + P);
-        System.out.println("r=" + r);
-        System.out.println("n=" + n);
-        double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
 
-        return M;
+        double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+        this.monthlyPayment = M;
     }
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
+
+    public static void main(String[] args) {
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
+    }
+
 }
